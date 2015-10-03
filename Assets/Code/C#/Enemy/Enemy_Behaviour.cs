cd4c7b2306058f3ce8a118b8489cddd7e29de61d
rcd315 <bellotti.rafael@gmail.com>
2015-10-03 19:57:19 -0400

    Stage2

---
+++ Assets/Code/C#/Enemy/Enemy_Behaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Behaviour : MonoBehaviour {
+
+	Rigidbody2D enemyBody;
+	
+	void Start () 
+	{
+		enemyBody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void move()
+	{
+		//enemyBody.velocity = Vector2
+	}
+
+	void steppedOn()
+	{
+
+	}
+	
+}
