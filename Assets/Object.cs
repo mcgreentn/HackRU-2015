76ea5967851ecb18447f270aa48b93be43c21a36
Taijizhou <taz218@lehigh.edu>
2015-10-04 09:32:19 -0400

    Made changes to menu

---
+++ Assets/Object.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Object : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Action_LevelSelection()
+    {
+        Application.LoadLevel("stages");
+    }
+
+    public void Action_QuitGame()
+    {
+        Application.Quit();
+    }
+}
