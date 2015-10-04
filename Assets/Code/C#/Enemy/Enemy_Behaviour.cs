717c5c23e6745371c0694435cc83d56175d7b590
rcd315 <bellotti.rafael@gmail.com>
2015-10-03 20:27:04 -0400

    Enemy stuff

--- Assets/Code/C#/Enemy/Enemy_Behaviour.cs
+++ Assets/Code/C#/Enemy/Enemy_Behaviour.cs
@@ -4,21 +4,36 @@ using System.Collections;
 public class Enemy_Behaviour : MonoBehaviour {
 
 	Rigidbody2D enemyBody;
+	bool moveXFlag = true;
 	
 	void Start () 
 	{
 		enemyBody = this.GetComponent<Rigidbody2D>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		move ();
+	}
+
+	void  OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "Jump_Platform") {
+			moveXFlag = !moveXFlag;
+		}
+
+		if (col.gameObject.name == "Sphere")
+			Destroy (col.gameObject);
 	}
 
 	void move()
 	{
-		//enemyBody.velocity = Vector2
+		if (moveXFlag == true)
+			enemyBody.velocity = new Vector2(1.0f,0.0f);
+		else
+			enemyBody.velocity = new Vector2(-1.0f,0.0f);
 	}
 
 	void steppedOn()
